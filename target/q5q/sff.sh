5bd9c8a71b425cc65873782cb740373c8eb775e8
GoRhanHee <gorhanhee01@gmail.com>
2026-05-13 21:58:44 +0900

    target: q5q: init
    Signed-off-by: GoRhanHee <gorhanhee01@gmail.com>

---
+++ target/q5q/sff.sh
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2025 Salvo Giangreco
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# SEC Floating Feature configuration file for Galaxy Z Fold5 (q5q)
+
+# Camera
+SEC_FLOATING_FEATURE_CAMERA_CONFIG_CAMID_TELE2=-1
+SEC_FLOATING_FEATURE_CAMERA_CONFIG_NIGHT_FRONT_DISPLAY_FLASH_TRANSPARENT=50
+SEC_FLOATING_FEATURE_CAMERA_CONFIG_UW_DISTORTION_CORRECTION=0,109,2,23060701,4000,3000,1,0,60,0
+SEC_FLOATING_FEATURE_CAMERA_CONFIG_WIDE_DISTORTION_CORRECTION=0,0,0,0,0,0,0,0,0,0
+
+# Display
+SEC_FLOATING_FEATURE_LCD_CONFIG_AOD_REFRESH_RATE=1
+
+# S Pen
+SEC_FLOATING_FEATURE_COMMON_CONFIG_SPEN_SENSITIVITY_ADJUSTMENT=0
+SEC_FLOATING_FEATURE_FRAMEWORK_CONFIG_SPEN_VERSION=0
+
+# Device specific
+TARGET_LCD_CONFIG_HFR_MODE="3"
+TARGET_LCD_CONFIG_SUB_HFR_MODE="2"
+TARGET_LCD_CONFIG_SUB_HFR_SUPPORTED_REFRESH_RATE="24,30,48,60,96,120"
